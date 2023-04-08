71d5ea91a6993f10971236910fa8b8b0dedf1a90
Harsh Singh Kushwaha <80638077+harsh774@users.noreply.github.com>
2023-04-08 18:56:24 +0530

    BST Concepts

--- BinaryTreeB.java
+++ BinaryTreeB.java
@@ -13,100 +13,206 @@ public class BinaryTreeB {
         }
     }
 
-    static class BinaryTree {
-        static int idx = -1;
+    // static class BinaryTree {
+    //     static int idx = -1;
 
-        public static Node buildTree(int nodes[]) {
-            idx++;
-            if (nodes[idx] == -1) {
-                return null;
-            }
+    //     public static Node buildTree(int nodes[]) {
+    //         idx++;
+    //         if (nodes[idx] == -1) {
+    //             return null;
+    //         }
 
-            Node newNode = new Node(nodes[idx]);
-            newNode.left = buildTree(nodes);
-            newNode.right = buildTree(nodes);
+    //         Node newNode = new Node(nodes[idx]);
+    //         newNode.left = buildTree(nodes);
+    //         newNode.right = buildTree(nodes);
 
-            return newNode;
+    //         return newNode;
+    //     }
+
+    //     public static void preorder(Node root) {
+    //         if (root == null) {
+    //             return;
+    //         }
+    //         System.out.print(root.data);
+    //         preorder(root.left);
+    //         preorder(root.right);
+    //     }
+
+    //     public static void inorder(Node root) {
+    //         if (root == null) {
+    //             return;
+    //         }
+    //         inorder(root.left);
+    //         System.out.print(root.data + " ");
+    //         inorder(root.right);
+    //     }
+
+    //     public static void levelOrder(Node root) {
+    //         if (root == null) {
+    //             return;
+    //         }
+
+    //         Queue<Node> q = new LinkedList<>();
+    //         q.add(root);
+    //         q.add(null);
+
+    //         while (!q.isEmpty()) {
+    //             Node currNode = q.remove();
+    //             if (currNode == null) {
+    //                 System.out.println();
+    //                 if (q.isEmpty()) {
+    //                     break;
+    //                 } else {
+    //                     q.add(null);
+    //                 }
+    //             } else {
+    //                 System.out.print(currNode.data + " ");
+    //                 if (currNode.left != null) {
+    //                     q.add(currNode.left);
+    //                 }
+    //                 if (currNode.right != null) {
+    //                     q.add(currNode.right);
+    //                 }
+    //             }
+    //         }
+    //     }
+
+    // }
+
+    // public static boolean isIdentical(Node node, Node subRoot) {
+    //     if (node == null && subRoot == null) {
+    //         return true;
+    //     } else if (node == null || subRoot == null || node.data != subRoot.data) {
+    //         return false;
+    //     }
+
+    //     if (!isIdentical(node.left, subRoot.left)) {
+    //         return false;
+    //     }
+    //     if (!isIdentical(node.right, subRoot.right)) {
+    //         return false;
+    //     }
+    //     return true;
+    // }
+
+    // public static boolean isSubtree(Node root, Node subRoot) {
+    //     if (root == null) {
+    //         return false;
+    //     }
+
+    //     if (root.data == subRoot.data) {
+    //         if (isIdentical(root, subRoot)) {
+    //             return true;
+    //         }
+    //     }
+
+    //     return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    // }
+    static class Info{
+        Node node;
+        int hd;
+
+        public Info(Node node, int hd){
+            this.hd = hd;
+            this.node = node;
         }
+    }
+    public static void TopView(Node root){
+        //level order traversal
+        Queue<Info> q = new LinkedList<>();
+        HashMap<Integer,Node> map = new HashMap<>();
 
-        public static void preorder(Node root) {
-            if (root == null) {
-                return;
-            }
-            System.out.print(root.data);
-            preorder(root.left);
-            preorder(root.right);
-        }
+        int min=0, max=0;
+        q.add(new Info(root,0));
+        q.add(null);
 
-        public static void inorder(Node root) {
-            if (root == null) {
-                return;
-            }
-            inorder(root.left);
-            System.out.print(root.data + " ");
-            inorder(root.right);
-        }
-
-        public static void levelOrder(Node root) {
-            if (root == null) {
-                return;
-            }
-
-            Queue<Node> q = new LinkedList<>();
-            q.add(root);
-            q.add(null);
-
-            while (!q.isEmpty()) {
-                Node currNode = q.remove();
-                if (currNode == null) {
-                    System.out.println();
-                    if (q.isEmpty()) {
-                        break;
-                    } else {
-                        q.add(null);
-                    }
-                } else {
-                    System.out.print(currNode.data + " ");
-                    if (currNode.left != null) {
-                        q.add(currNode.left);
-                    }
-                    if (currNode.right != null) {
-                        q.add(currNode.right);
-                    }
+        while(!q.isEmpty()){
+            Info curr = q.remove();
+            if(curr==null){
+                if(q.isEmpty()){
+                    break;
+                }else{
+                    q.add(null);
+                }
+            }else{
+                if(!map.containsKey(curr.hd)){
+                    map.put(curr.hd, curr.node);
+                }
+    
+                if(curr.node.left!=null){
+                    q.add(new Info(curr.node.left, curr.hd-1));
+                    min=Math.min(min, curr.hd-1);
+                }
+    
+                if(curr.node.right!=null){
+                    q.add(new Info(curr.node.right, curr.hd+1));
+                    max=Math.max(max, curr.hd+1);
                 }
             }
         }
 
+        for(int i=min; i<=max; i++){
+            System.out.print(map.get(i).data+" ");
+        }
+        System.out.println();
     }
 
-    public static boolean isIdentical(Node node, Node subRoot) {
-        if (node == null && subRoot == null) {
+    public static void kLevelNodes(Node root, int level, int k){
+        if(k==level){
+            System.out.print(root.data+" ");
+            return;
+        }
+        kLevelNodes(root.left, level+1, k);
+        kLevelNodes(root.right, level+1, k);
+    }
+    public static boolean getPath(Node root, int n, ArrayList<Node> path){
+        if(root==null){
+            return false;
+        }
+        path.add(root);
+        if(root.data==n){
             return true;
-        } else if (node == null || subRoot == null || node.data != subRoot.data) {
-            return false;
         }
+        boolean fuleft = getPath(root.left, n, path);
+        boolean furight = getPath(root.right, n, path);
 
-        if (!isIdentical(node.left, subRoot.left)) {
-            return false;
+        if(fuleft || furight){
+            return true;
         }
-        if (!isIdentical(node.right, subRoot.right)) {
-            return false;
-        }
-        return true;
+        path.remove(path.size()-1);
+        return false;
     }
 
-    public static boolean isSubtree(Node root, Node subRoot) {
-        if (root == null) {
-            return false;
-        }
+    public static Node lca(Node root, int n1, int n2){ //O(n)
+        ArrayList<Node> path1  = new ArrayList<>();
+        ArrayList<Node> path2 = new ArrayList<>();
 
-        if (root.data == subRoot.data) {
-            if (isIdentical(root, subRoot)) {
-                return true;
+        getPath(root, n1,path1);
+        getPath(root,n2,path2);
+
+        int i=0;
+        for(; i<path1.size() && i<path2.size(); i++){
+            if(path1.get(i)!=path2.get(i)){
+                break;
             }
         }
 
-        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+        Node lca = path1.get(i-1);
+        return lca;
+    }
+
+    public static boolean uniValued(Node root){
+        if(root == null) return true;
+        if(root.left!=null && root.data!=root.left.data){
+            return false;
+        }
+        if(root.right!=null && root.data!=root.right.data){
+            return false;
+        }
+        return uniValued(root.left) && uniValued(root.right);
+
+      
+
     }
 
     public static void main(String[] args) {
@@ -121,14 +227,27 @@ public class BinaryTreeB {
         root.right = new Node(3);
         root.left.left = new Node(4);
         root.left.right = new Node(5);
-        root.right.right = new Node(6);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
 
-        Node subRoot = new Node(2);
-        subRoot.left = new Node(4);
-        subRoot.right = new Node(5);
+        // Node subRoot = new Node(2);
+        // subRoot.left = new Node(4);
+        // subRoot.right = new Node(5);
         // subRoot.left.left = new Node(6);
 
-        System.out.println(isSubtree(root, subRoot));
+        // System.out.println(isSubtree(root, subRoot));
+        // TopView(root);
+        // kLevelNodes(root, 1, 3);
+        // System.out.println(lca(root, 4, 5).data);
+        Node root2 = new Node(1);
+        root2.left = new Node(1);
+        root2.right = new Node(1);
+        root2.left.left = new Node(1);
+        root2.left.right = new Node(1);
+        root2.right.right = new Node(1);
+        System.out.println(uniValued(root2));
+
+
         
         
     }
---
+++ bst.java
@@ -0,0 +1,160 @@
+import java.util.*;
+
+public class bst {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data = data;
+        }
+    }
+
+    public static Node insert(Node root, int val){
+        if(root==null){
+            root = new Node(val);
+            return root;
+        }
+
+        if(root.data > val){
+            root.left = insert(root.left, val);
+        }else{
+            root.right = insert(root.right, val);
+        }
+        return root;
+    }
+    public static void inorder(Node root){
+        if(root==null){
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data+" ");
+        inorder(root.right);
+    }
+
+    public static boolean search(Node root, int key){ //O(H)
+        if(root==null){
+            return false;
+        }
+        if(root.data==key){
+            return true;
+        }
+
+        if(root.data>key){
+            return search(root.left, key);
+        }else{
+            return search(root.right, key);
+        }
+    }
+
+    public static Node delete(Node root, int val){
+        if(root.data<val){
+            root.right = delete(root.right, val);
+        }else if(root.data>val){
+            root.left = delete(root.left, val);
+        }else{
+            //case1 leaf node
+            if(root.left==null && root.right==null){
+                return null;
+            }
+
+            //case2 one child
+            if(root.left==null){
+                return root.right;
+            }else if(root.right==null){
+                return root.left;
+            }
+
+            //case3 both children
+            Node IS = findInorderSuccessor(root.right);
+            root.data = IS.data;
+            root.right = delete(root.right, IS.data);
+        }
+
+        return root;
+    }
+
+    public static Node findInorderSuccessor(Node root){
+        while(root.left!=null){
+            root=root.left;
+        }
+        return root;
+    }
+
+    public static void printInRange(Node root, int k1, int k2){
+        if(root==null){
+            return;
+        }
+        
+        if(root.data >= k1 && root.data <= k2){
+            printInRange(root.left, k1, k2);
+            System.out.print(root.data+" ");
+            printInRange(root.right, k1, k2);
+        }else if(root.data<k1){
+            printInRange(root.left, k1, k2);
+        }else{
+            printInRange(root.right, k1, k2);
+        }
+    }
+
+    public static void printPathtoLeaf(ArrayList<Integer> path){
+        for(int i: path){
+            System.out.print(i+"->");
+        }
+        System.out.println("Null");
+    }
+    public static void printPath(Node root, ArrayList<Integer> path){
+        if(root == null){
+            return;
+        }
+        path.add(root.data);
+        if(root.left==null && root.right==null){
+            printPathtoLeaf(path);
+        }
+        printPath(root.left, path);
+        printPath(root.right, path);
+        path.remove(path.size()-1);
+
+    }
+    public static boolean isValidBST(Node root, Node min, Node max){
+        if(root == null) return true;
+        
+        if(min!=null && root.data <= min.data){
+            return false;
+        }else if(max!=null && root.data >= max.data){
+            return false;
+        }
+
+        return isValidBST(root.left, min, root) && isValidBST(root.right, root, max);
+    }
+
+
+    public static void main(String[] args) {
+        int val[] = {8,5,3,1,4,6,10,11,14};
+        // int val[] = {1,1,1};
+        Node root = null;
+        for(int i:val){
+            root = insert(root, i);
+        }
+
+        // inorder(root);
+        // System.out.println();
+
+        // System.out.println(search(root, 10));
+        // System.out.println(search(root, 12));
+
+
+        // root = delete(root, 5);
+        // System.out.println();
+        // inorder(root);
+
+        // printInRange(root, 0, 11);
+        // printPath(root, new ArrayList<>());
+        // if(isValidBST(root, null, null)){
+        //     System.out.println("Valid");
+        // }else{
+        //     System.out.println("Not Valid");
+        // }
+    }
+}  
