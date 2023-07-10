5960ce47fe9cd297ca7ff7c25b58d7603ae8076d
Harsh Singh Kushwaha <80638077+harsh774@users.noreply.github.com>
2023-07-10 11:59:53 +0530

    Graph completed.

---
+++ graphrevision/RevisionClass.java
@@ -0,0 +1,29 @@
+package graphrevision;
+
+import java.util.ArrayList;
+
+public class RevisionClass {
+    static class Edge {
+        int src;
+        int dest;
+        int wt;
+
+        public Edge(int s, int d, int wt) {
+            this.src = s;
+            this.dest = d;
+            this.wt = wt;
+        }
+    }
+
+    public static void createGraph(ArrayList<E> graph){
+        for(int i=0; i<graph.length; i++){
+            gra
+        }
+    }
+
+    public static void main(String[] args) {
+        // BFS
+        ArrayList<Edge>[] graph = new ArrayList[V];
+        createGraph(graph);
+    }
+}
